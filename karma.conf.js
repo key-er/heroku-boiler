70261439b4d965c9562b452ebb268037693177ab
key-er <nasaquba@gmail.com>
2018-08-15 13:52:40 -0700

    reporting works + refactored  App to a sep file b.c testing file with ReactDOM.render gives error

--- karma.conf.js
+++ karma.conf.js
@@ -1,237 +1,53 @@
 // Karma configuration
-// Generated on Mon Aug 13 2018 10:35:38 GMT-0700 (PDT)
-
-
-// module.exports = function(config) {
-//   config.set({
-
-//     // base path that will be used to resolve all patterns (eg. files, exclude)
-//     basePath: '',
-
-
-//     // frameworks to use
-//     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-//     frameworks: ['mocha', 'chai'],
-
-
-//     // list of files / patterns to load in the browser
-//     files: [
-//       'test/**/*.js',
-//        'react-client/src/*.js'
-//     ],
-
-
-//     // list of files / patterns to exclude
-//     exclude: [
-//     ],
-
-
-//     // preprocess matching files before serving them to the browser
-//     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-//     preprocessors: {
-//         'react-client/src/*.js' : 'coverage'
-//     },
-
-
-//     // test results reporter to use
-//     // possible values: 'dots', 'progress'
-//     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-//     reporters: ['mocha', 'progress', 'coverage'],
-
-
-//     // web server port
-//     port: 9876,
-
-
-//     // enable / disable colors in the output (reporters and logs)
-//     colors: true,
-
-
-//     // level of logging
-//     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-//     logLevel: config.LOG_INFO,
-
-
-//     // enable / disable watching file and executing tests whenever any file changes
-//     autoWatch: true,
-
-
-//     // start these browsers
-//     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-//     browsers: ['Chrome'],
-
-
-//     // Continuous Integration mode
-//     // if true, Karma captures browsers, runs the tests and exits
-//     singleRun: false,
-
-//     // Concurrency level
-//     // how many browser should be started simultaneous
-//     concurrency: Infinity,
-
-
-//     coverageReporter: {
-//       type : 'html',
-//       dir : 'coverage/'
-//     }
-
-//     })
-// }
-
-
-
-// **************************************
-// **************************************
-// **************************************
-// **************************************
-// **************************************
-// **************************************
-
-
-
-
-
-// var path = require('path');
-// var SRC_DIR = path.join(__dirname, '../react-client/src');
-// var DIST_DIR = path.join(__dirname, '../react-client/dist');
-
-// var webpack = require('webpack')
-// module.exports = function (config) {
-//   config.set({
-//     browsers: [ 'Chrome' ],
-//     // karma only needs to know about the test bundle
-//     files: [
-//       // 'tests.bundle.js',
-//       { pattern: 'test/**/*.js', watched: false },
-//       { pattern: 'react-client/src/*.js', watched: true},
-//       { pattern: 'react-client/src/**/*.spec.js', watched: true},
-//     ],
-
-//     frameworks: [ 'chai', 'mocha' ],
-//     // plugins: [
-//     //   'karma-chrome-launcher',
-//     //   'karma-chai',
-//     //   'karma-mocha',
-//     //   'karma-sourcemap-loader',
-//     //   'karma-webpack',
-//     // ],
-//     // run the bundle through the webpack and sourcemap plugins
-//     preprocessors: {
-//       'test/**/*.js': ['babel', 'webpack'],
-//       'react-client/**/*.js': 'coverage',
-//       'react-client/src/**/*.spec.js' : ['webpack', 'babel'],
-//       'react-client/src/**/*.jsx' : ['webpack', 'babel']
-//     },
-
-//     reporters: ['mocha', 'progress', 'coverage'],
-
-//     coverageReporter: {
-//       type : 'html',
-//       dir : 'coverage/'
-//     },
-
-//     // Concurrency level
-//     // how many browser should be started simultaneous
-//     concurrency: Infinity,
-
-//     // web server port
-//     port: 9876,
-
-
-//     // enable / disable colors in the output (reporters and logs)
-//     colors: true,
-
-
-//     singleRun: false,
-//     // webpack config object
-
-//     webpack: {
-//       // devtool: 'inline-source-map',
-//       module: {
-//       loaders : [
-//         {
-//           test : /\.jsx?/,
-//           include : '/react-client/src',
-//           loader : 'babel-loader',
-//           query: {
-//             presets: ['react', 'es2015']
-//          }
-//         }
-//       ],
-
-//       }
-//     },
-
-
-
-//     // webpack: {
-//     //   entry: `${SRC_DIR}/index.jsx`,
-//     //   output: {
-//     //     filename: 'bundle.js',
-//     //     path: DIST_DIR
-//     //   },
-//     //   module : {
-//     //     loaders : [
-//     //       {
-//     //         test : /\.jsx?/,
-//     //         include : SRC_DIR,
-//     //         loader : 'babel-loader',
-//     //         query: {
-//     //           presets: ['react', 'es2015']
-//     //        }
-//     //       }
-//     //     ]
-//     //   }
-//     // },
-
-//     webpackMiddleware: {
-//       noInfo: true,
-//     }
-//   });
-// };
-
-
-
-
-
-
-
-
-
-// ***********************************************
-// ***********************************************
-// ***********************************************
-// **************************************
-// **************************************
-
-
 
 var webpackConfig = require('./webpack.config.js')
-webpackConfig.entry = null;
-
+webpackConfig.entry = null; // explicitly setting up config to null
 
 module.exports = function (config) {
   config.set({
-    browsers: [ 'Chrome' ], //run in Chrome
-    singleRun: true, //just run once by default
-    frameworks: [ 'mocha' , 'chai'], //use the mocha test framework
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: [ 'Chrome' ],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: [ 'mocha' , 'chai'],
+
+    // enable or disable colors in the output (reporters and logs)
+    colors: true,
+
+    // list of files / patterns to load in the browser
     files: [
-      // 'tests/clientSpecs/List.js' //just load this file
       'react-client/src/components/**/*.jsx',
       { pattern: 'tests/clientSpecs/**/*.js', watched: true }
     ],
 
+
+    // list of files / patterns to exclude
+    exclude: [
+    ],
+
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity,
+
+
     reporters: [ 'progress', 'coverage' ],
 
     preprocessors: {
-      'react-client/src/components/**/': 'webpack', //preprocess with webpack and our sourcemap loader
-      'tests/clientSpecs/**/*.js': [ 'webpack'], //preprocess with webpack and our sourcemap loader
+      'react-client/src/components/**/*.jsx': ['webpack', 'coverage'],
+      'tests/clientSpecs/**/*.js': [ 'webpack'] //preprocess with webpack and our sourcemap loader
 
     },
      //report results in this format
     webpack: webpackConfig,
-
-    reporters: ['progress', 'coverage'],
 
     coverageReporter: {
       type : 'html',
@@ -243,6 +59,17 @@ module.exports = function (config) {
     },
 
     singleRun: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
   });
 };
 
--- react-client/src/components/ListItem.jsx
+++ react-client/src/components/ListItem.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
-// const ListItem = (props) => (
-//   <div>
-//     { props.item.description }
-//   </div>
-// )
-
-
-class ListItem extends React.Component {
-  render() {
-    <div>
-      { props.item.description }
-    </div>
-  }
-}
-
-
+const ListItem = (props) => (
+  <div>
+    { props.item.description }
+  </div>
+)
 
 export default ListItem;
